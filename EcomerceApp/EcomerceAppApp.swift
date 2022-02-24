764d5b3131d80479593094dbd8c2e6319337de91
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-24 15:34:42 +0530

    Initial Commit

---
+++ EcomerceApp/EcomerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcomerceAppApp.swift
+//  EcomerceApp
+//
+//  Created by shehan karunarathna on 2022-02-24.
+//
+
+import SwiftUI
+
+@main
+struct EcomerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
